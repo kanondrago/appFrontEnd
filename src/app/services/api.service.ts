52c45453bdeda1c172a25f0728c53dd6b37f3d42
kanondrago <ronaldchavezr@gmail.com>
2023-09-29 20:37:54 +0200

    api service with Login Logout and LocalStorage

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+
 import { User } from '../models/User';
+import { UserLogin } from '../interfaces/UserLogin';
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,15 +16,41 @@ export class ApiService {
 
   private urlApi = 'http://localhost:3000/';
 
-  
+  private userSubject = new BehaviorSubject<User>(this.getUserLocalStorage());
+  public userObservable:Observable<User>;
 
-  constructor(private http: HttpClient) { }
-
-  public login() {
+  // private toastrService: ToastrService
+  constructor(
+    private http: HttpClient,
+    private toastrService: ToastrService,
+    private router: Router) 
+  { 
+    // Obtener en información para el Header
+    this.userObservable = this.userSubject.asObservable();
   }
 
-  public logout() {
+  public login(userLogin: UserLogin): Observable<User> {
+    return this.http.post<User>('http://localhost:3000/login', userLogin).pipe(
+      tap({
+        next: (user) => {
+          this.setUserLocalStorage(user);
+          this.userSubject.next(user);
+          this.toastrService.success(
+            `Bienvenidos a Vacamarca ${user.username}`,
+            'Logeo exitoso'
+          )
+        },
+        error: (errorResponse) => {
+          this.toastrService.error(errorResponse.error, 'Logeo Fallido');
+        }
+      })
+    ) 
+  } 
 
+  public logout() {
+    this.userSubject.next(new User());
+    localStorage.removeItem('user');
+    this.router.navigateByUrl('/login');
   }
 
   // Seteo del LocalStorage
