cef8c192c3173ff01e1bd9d323abd7124b45f285
kanondrago <ronaldchavezr@gmail.com>
2023-10-04 17:46:40 +0200

    Consumo de API USER y CUSTOMER completado

--- src/app/pages/customer/customer.component.ts
+++ src/app/pages/customer/customer.component.ts
@@ -1,25 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog'
 import { TransaccionComponent } from 'src/app/components/transaccion/transaccion.component';
 import { DataService } from 'src/app/services/data.service';
+
+// Modelos
+import { Transaction } from 'src/app/models/Transaction';
 
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
   styleUrls: ['./customer.component.css']
 })
-export class CustomerComponent {
+export class CustomerComponent implements OnInit {
+
+  transacciones: Transaction[];
+  totalTransacciones=0;
 
   constructor(private diagloRef: MatDialog, private dataService: DataService) {
 
     this.dataService.getUser().subscribe(data => {
       console.log(data);
+
+      this.transacciones = data['data'].transactions;
+
+      console.log(this.transacciones);
+
+      this.totalTransacciones = this.sumarTransacciones();
+
+      console.log(this.totalTransacciones);
     })
 
+  }
+  ngOnInit(): void {
+    
   }
 
   openDialog() {
     this.diagloRef.open(TransaccionComponent);
+  }
+
+  sumarTransacciones(): number {
+    let suma = 0;
+    this.transacciones.forEach((valor => {      
+      suma = suma + valor.amount;    
+    }))
+    
+    return suma;
   }
 
 }
