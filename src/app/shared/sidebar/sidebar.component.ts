deaa30a1dde1f85920864f28c337f1f168755f2d
kanondrago <ronaldchavezr@gmail.com>
2023-10-05 13:52:52 +0200

    SideBar tipo de usuario completo

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+// Servicios
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
+
+  token='';
+  tipoUsuario:string;
+  isAuthAdmin=false;
+  isAuthCustomer=false;
+
+  constructor(private dataService: DataService) {
+    const userJSON = localStorage.getItem('user');
+    const objeto = JSON.parse(userJSON);
+    this.token = objeto['auth'].token
+
+    this.dataService.getUser(this.token).subscribe(data => {
+      console.log('holaaaa')
+      console.log(data['data'].role.type);
+      console.log('holaaaa')
+
+      this.tipoUsuario = data['data'].role.type
+      console.log(this.tipoUsuario);
+      if(this.tipoUsuario==='admin'){
+        this.isAuthAdmin=true;
+      }
+      if(this.tipoUsuario==='customer'){
+        this.isAuthCustomer=true;
+      }
+    })
+  }
+
+
+
 
 }
